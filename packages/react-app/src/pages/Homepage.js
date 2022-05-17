322a17e6d2ae47f57792c6b4f61a70b7470b7c19
route-2 <ruthurao@gmail.com>
2022-05-17 19:44:30 +0530

    first commit

---
+++ packages/react-app/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css'
+
+function HomePage() {
+    return ( 
+        <div className='cont'>
+            <p className='header' >Welcome <br/>To <br/><span className='purple'>ReKarma</span> </p>
+        </div>
+
+     );
+}
+
+export default HomePage;
